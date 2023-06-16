de6a6c930690f223d86c0fcca5bb2ea881f1cb74
aldyisza87 <57266292+aldyisza87@users.noreply.github.com>
2023-06-16 16:53:47 +0700

    create home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:qr_code/app/controllers/auth_controller.dart';
+import 'package:qr_code/app/routes/app_pages.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  HomeView({Key? key}) : super(key: key);
+
+  final AuthController authC = Get.find<AuthController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,11 +18,75 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: GridView.builder(
+          padding: const EdgeInsets.all(20),
+          itemCount: 4,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+          ),
+          itemBuilder: (context, index) {
+            late String title;
+            late IconData icon;
+            late VoidCallback ontap;
+
+            switch (index) {
+              case 0:
+                title = "Add Products";
+                icon = Icons.post_add_rounded;
+                ontap = () => Get.toNamed(Routes.addProduct);
+                break;
+              case 1:
+                title = "Products";
+                icon = Icons.list_alt_rounded;
+                ontap = () => Get.toNamed(Routes.detailProduct);
+                break;
+              case 2:
+                title = "QR Code";
+                icon = Icons.qr_code_2_rounded;
+                ontap = () => print("Open Camera");
+                break;
+              case 3:
+                title = "Catalogue";
+                icon = Icons.document_scanner_rounded;
+                ontap = () => print("Open Pdf");
+                break;
+              default:
+            }
+            return Material(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(14),
+              child: InkWell(
+                onTap: ontap,
+                borderRadius: BorderRadius.circular(14),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Icon(icon),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(title)
+                  ],
+                ),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Map<String, dynamic> hasil = await authC.logout();
+          if (hasil["error"] == false) {
+            Get.offAllNamed(Routes.login);
+          } else {
+            Get.snackbar("Error", hasil["error"]);
+          }
+        },
+        child: const Icon(Icons.logout),
       ),
     );
   }
