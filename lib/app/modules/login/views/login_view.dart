eedf9f3cc37354dd2ae8a29bd55544d761fbd8ae
aldyisza87 <57266292+aldyisza87@users.noreply.github.com>
2023-10-16 01:01:17 +0700

    last commit

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -17,7 +17,7 @@ class LoginView extends GetView<LoginController> {
     return Scaffold(
       extendBody: true,
       extendBodyBehindAppBar: true,
-      backgroundColor: Colors.blue[100],
+      backgroundColor: const Color(0xFF363062),
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
@@ -29,23 +29,22 @@ class LoginView extends GetView<LoginController> {
           const Text(
             "Login",
             style: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.w700,
-            ),
+                fontSize: 32, fontWeight: FontWeight.w700, color: Colors.white),
           ),
           const Text(
             "Sign In to Countinue",
             style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w400,
-                color: Color.fromARGB(255, 102, 98, 98)),
+                color: Colors.white60),
           ),
           const SizedBox(
             height: 64,
           ),
           const Text(
             "Email Address",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.w400, color: Colors.white),
           ),
           const SizedBox(
             height: 12,
@@ -55,18 +54,27 @@ class LoginView extends GetView<LoginController> {
             controller: emailC,
             keyboardType: TextInputType.emailAddress,
             decoration: InputDecoration(
-              prefixIcon: const Icon(Icons.email_outlined),
+              prefixIcon: const Icon(
+                Icons.email_outlined,
+                color: Colors.white,
+              ),
               hintText: "Your Email Address",
-              // labelText: "Email",
+              hintStyle: const TextStyle(color: Colors.white),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12)),
               border: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.white),
                 borderRadius: BorderRadius.circular(12),
               ),
             ),
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(height: 20),
           const Text(
             "Password",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.w400, color: Colors.white),
           ),
           const SizedBox(
             height: 12,
@@ -79,8 +87,19 @@ class LoginView extends GetView<LoginController> {
               obscureText: controller.isHidden.value,
               decoration: InputDecoration(
                 hintText: "Your Password",
+                hintStyle: const TextStyle(
+                  color: Colors.white,
+                ),
                 //labelText: "Password",
-                prefixIcon: const Icon(Icons.key_off_outlined),
+                prefixIcon: const Icon(
+                  Icons.key_off_outlined,
+                  color: Colors.white,
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                    )),
                 suffixIcon: IconButton(
                   onPressed: () {
                     controller.isHidden.toggle();
@@ -89,12 +108,14 @@ class LoginView extends GetView<LoginController> {
                     controller.isHidden.isFalse
                         ? Icons.remove_red_eye
                         : Icons.remove_red_eye_outlined,
+                    color: Colors.white,
                   ),
                 ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(9),
                 ),
               ),
+              style: const TextStyle(color: Colors.white),
             ),
           ),
           const SizedBox(height: 35),
@@ -112,16 +133,17 @@ class LoginView extends GetView<LoginController> {
                   } else {
                     Get.offAllNamed(Routes.home);
                   }
-                  // handle login status jika email "admin@gmail.com" maka tampilkan dashboard
+                  /*  // handle login status jika email "admin@gmail.com" maka tampilkan dashboard
                   if (emailC.text == "admin@gmail.com") {
                     Get.offAllNamed(Routes.home);
-                  }
+                  } */
                 } else {
                   Get.snackbar("Error", "Email dan password wajib diisi.");
                 }
               }
             },
             style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFF99417),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
